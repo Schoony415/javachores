9a47d8c8532f7ecf8bb8788da316a209c58ad71f
Schoony415 <joshua.schoonover@austincc.edu>
2021-02-02 09:42:33 -0600

    changing var names
    i = counter
    n = bignum

--- Application.java
+++ Application.java
@@ -4,9 +4,9 @@ public class Application {
     public static void main(String[] args){
         System.out.println("fuuuuuuuuuuuuuuuuuuuuuuuuuck java");
         System.out.println("like for real what is this crap, why did i make like a thousand files just to print to a conesole awwwwwwwwwwwwwww");
-        int n =10;
-        for (int i =0; i < n; i++) {
-            System.out.print(i);
+        int bignum =10;
+        for (int counter =0; counter < bignum; counter++) {
+            System.out.print(counter);
         }
         System.out.println("#count from 0");
         //line added for version control
--- Program.java
+++ Program.java
@@ -1,1 +1,2 @@
 class Program{public static void main(String[] args){System.out.println("Look mom it's on one line, don't tell them big wigs");}}
+//the definition of small code?
