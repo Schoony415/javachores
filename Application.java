c149cfb652e083656bb53c123943e1bea00663bf
Schoony415 <joshua.schoonover@austincc.edu>
2021-02-02 09:12:02 -0600

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,13 @@
+package com.galvanize;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("fuuuuuuuuuuuuuuuuuuuuuuuuuck java");
+        System.out.println("like for real what is this crap, why did i make like a thousand files just to print to a conesole awwwwwwwwwwwwwww");
+        int n =10;
+        for (int i =0; i < n; i++) {
+            System.out.print(i);
+        }
+        System.out.println("#count from 0");
+    }
+}
